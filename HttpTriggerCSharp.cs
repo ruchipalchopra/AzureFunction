abeca88210bff5f315a33153ec1a416f11be131b
“Ruchi <ruchichopra.in@gmail.com>
2020-04-21 15:22:05 +0800

    template for calling api using pAT

---
+++ HttpTriggerCSharp.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Company.Function
+{
+    public static class HttpTriggerCSharp
+    {
+        [FunctionName("HttpTriggerCSharp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+      
+        var personalaccesstoken = "";
+
+		using (HttpClient client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Accept.Add(
+				new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+				Convert.ToBase64String(
+					System.Text.ASCIIEncoding.ASCII.GetBytes(
+						string.Format("{0}:{1}", "", personalaccesstoken))));
+
+			using (HttpResponseMessage response = await client.GetAsync(
+						""))
+            {
+				response.EnsureSuccessStatusCode();
+				string responseMessage = await response.Content.ReadAsStringAsync();
+			
+            return new OkObjectResult(responseMessage);
+			}
+		}
+        }
+    }
+}
+
+
